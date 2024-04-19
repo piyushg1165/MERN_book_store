fb6512dd2538386319f8ed063898d660466c0d86
Piyush Gupta <piyushgupta1165@gmail.com>
2024-04-19 15:20:42 +0530

    fixed cors

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import {Book} from "./models/bookModel.js";
 import bodyParser from "body-parser";
 import booksRoutes from "./routes/booksRoutes.js";
+import cors from 'cors'
 
 const app = express();
 
