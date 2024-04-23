8185048fce492e8e672f31e9dd8ca33e31e3937f
Piyush Gupta <piyushgupta1165@gmail.com>
2024-04-23 17:37:37 +0530

    made frontend more graphical

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 function CreateBook() {
+
+  const [bookTitle, setBookTitle] = useState("");
+  const [bookAuthor, setBookAuthor] = useState("");
+  const [bookPublishYear, setBookPublishYear] = useState();
+
   const postBook = () => {axios.post('http://localhost:5555/books', {
-    title: 'Chara ghotala',
-    author: 'lalu yadav',
-    publishYear: '1999'
+    title: bookTitle,
+    author: bookAuthor,
+    publishYear: bookPublishYear
   })
   .then((response) => {
     console.log(response);
@@ -13,13 +18,31 @@ function CreateBook() {
   .catch((error) => {
     console.log(error);
   })
+
+ 
 }
   return (
-    <div>
-      <button onClick={postBook}>click</button>
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
+    <div className='flex flex-col p-5 m-5 '>
+      <h1 className='place-self-center text-lg'>Book Title</h1>
+      <input type='text' 
+      name='booktitle'
+      className='m-2 p-2 border-solid border-2 border-sky-500'
+      value={bookTitle}
+      onChange={(e) => setBookTitle(e.target.value)}/>
+      <h1 className='place-self-center text-lg'>Book Author</h1>
+      <input type='text' 
+      name='bookauthor'
+      className='m-2 p-2 border-solid border-2 border-sky-500'
+      value={bookAuthor}
+      onChange={(e) => setBookAuthor(e.target.value)}/>
+      <h1 className='place-self-center text-lg'>Book Publish Year</h1>
+      <input type='text' 
+      name='bookyear'
+      className='m-2 p-2 border-solid border-2 border-sky-500'
+      value={bookPublishYear}
+      onChange={(e) => setBookPublishYear(e.target.value)}/>
+      <button className='text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-80 place-self-center' onClick={postBook}>Submit</button>
+
     </div>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,14 +17,19 @@ function Home() {
   
 
   return (
-    <div>
-      <h1>Welcome to React!</h1>
+    <div className='w-full items-center flex flex-col'>
+      <h1 className='text-5xl'>MERN Book Store</h1>
       
-      <ul>
+      <ul className="m-5 text-lg font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white w-4/5">
        {books.map(book => (
-        <li key={book._id}>{book.title} by {book.author} published in {book.publishYear}</li>
+        <li className="w-full px-4 py-2 border-b border-gray-200 rounded-t-lg dark:border-gray-600 flex flex-row m-5 gap-5" key={book._id}>
+          <div><h1 className='font-light'>Name</h1>{book.title}</div>
+          <div><h1 className='font-light'>Author</h1>{book.author}</div>
+          <div><h1 className='font-light'>Publish Year</h1>{book.publishYear}</div>
+          </li>
        ))}
       </ul>
+
     </div>
   )
 }
